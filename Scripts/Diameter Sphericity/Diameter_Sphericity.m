a88beda2e851c46c06581af1166318b201342462
YixuanChen0726 <156203475+YixuanChen0726@users.noreply.github.com>
2024-02-23 02:44:06 +0800

    Add files via upload

---
+++ Scripts/Diameter Sphericity/Diameter_Sphericity.m
@@ -0,0 +1,35 @@
+% Compute Diameter Sphericity
+clear all;
+
+%load cell name and its corresponding index
+NameDic=table2cell(readtable('..\bin\name_dictionary.csv'));
+
+%The Sample number of embryo
+SampleNum=4;
+VolumeData=readcell(['..\Surface Volume\Sample',num2str(SampleNum,'%02d'),'_BoundaryVolume.csv']);
+VolumeData(cellfun(@(x) any(ismissing(x)),VolumeData))={[]};
+%load OBB box,a,b,c
+load(['..\OBB Box\Sample',num2str(SampleNum,'%02d'),'_OBBbox.mat']);
+
+%Head of column: Sample Number, Cell Name, Cell Index, Frame, diameter sphericity
+Dsphericity=cell(size(OBBbox,1),5);Dsphericity(:,1:4)=OBBbox(:,1:4);
+%Create a cell to store diameter sphericity
+DSphtable=cell(size(VolumeData));
+DSphtable(1,:)=VolumeData(1,:);DSphtable(:,1)=VolumeData(:,1);
+for VariNum=1:size(OBBbox,1)
+    CellName=OBBbox{VariNum,2};CellIndex=OBBbox{VariNum,3};Timepoint=OBBbox{VariNum,4};
+    %The triaxial axis
+    a=OBBbox{VariNum,5};b=OBBbox{VariNum,7};c=OBBbox{VariNum,9};
+    [Namerow,Namecol]=find(cellfun(@(x) strcmp(x,CellName),VolumeData(1,:)));
+    V=VolumeData{Timepoint+1,Namecol};
+    %Put the data into cell and table respectively
+    DS=((6*V/pi).^(1/3))./a;
+    DSphtable{Timepoint+1,Namecol}=DS;
+    Dsphericity{VariNum,5}=DS;
+end
+
+%save to csv and mat
+writecell(DSphtable,['.\Sample',num2str(SampleNum,'%02d'),'_DSphericity.csv']);
+save(['.\Sample',num2str(SampleNum,'%02d'),'_Dsphericity.mat'],'Dsphericity','-v7.3');
+
+
---
+++ Scripts/Diameter Sphericity/ReadMe.txt
@@ -0,0 +1,10 @@
+Diameter Sphericity
+
+Script: Diameter_Sphericity.m
+Input: 
+(i) name_dictionary.csv
+(ii) cell volume, ..\Surface Volume\Sample*_BoundaryVolume.csv
+(iii) the triaxial axis of OBB box, ..\OBB Box\Sample*_OBBbox.mat
+Output: 
+(i) Sample*_DSphericity.csv
+(ii) Sample*_DSphericity.mat, contain the sample number, cell name, cell index, frame and Diameter Sphericity
